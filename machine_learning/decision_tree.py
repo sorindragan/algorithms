16f6c17b3ab34a49ec8d12dc0bf671a3a970f952
sorindragan <sorin.dragan27@gmail.com>
2020-02-07 01:26:39 +0100

    decision tree done

--- machine_learning/decision_tree.py
+++ machine_learning/decision_tree.py
@@ -3,16 +3,16 @@ import math
 class DecisionNode:
 
     def __init__(self, col=-1, value=None, labels=None, lb=None, rb=None):
-        self.col = col
-        self.value = value
-        self.labels = labels
-        self.left_branch = lb # true branch
+        self.col          = col
+        self.value        = value
+        self.labels       = labels
+        self.left_branch  = lb # true branch
         self.right_branch = rb # false branch
 
 def divide_set(rows, column, value):
     split_function = None
     if isinstance(value, int) or isinstance(value, float):
-        split_function = lambda row:row[column] >= value
+        split_function = lambda row: row[column] >= value
     else:
         split_function = lambda row: row[column] == value
 
@@ -21,18 +21,18 @@ def divide_set(rows, column, value):
 
     return set1, set2
 
-def count_labels(rows):
+def create_labels_dict(rows):
     labels = {}
     for row in rows:
         label = row[-1]
         if label not in labels:
             labels[label] = 1
         else:
-            labels[label] +=1
+            labels[label] += 1
     return labels
 
 def entropy(rows):
-    labels = count_labels(rows)
+    labels = create_labels_dict(rows)
     entropy = 0
     for l in labels.keys():
         p = float(labels[l]) / len(rows)
@@ -42,35 +42,77 @@ def entropy(rows):
 def construct_tree(rows, score_func=entropy):
     if len(rows) == 0:
         return DecisionNode()
+    
+    # calculate entropy before split
     score = score_func(rows)
 
     max_gain = 0
     split_criteria = None
     best_sets = None
 
-    column_no = len(rows[0]) - 1
-    for col in range(column_no):
+    for col in range(len(rows[0]) - 1):
         for val in [v[col] for v in rows]:
             set1, set2 = divide_set(rows, col, val)
-
             p = float(len(set1)) / len(rows)
             gain = score - p * score_func(set1) - (1-p) * score_func(set2)
+            
             if gain > max_gain and set1 and set2:
                 max_gain = gain
                 split_criteria = col, val
                 best_sets = set1, set2
+    
     if max_gain > 0:
         leftBranch = construct_tree(best_sets[0])
         rightBranch = construct_tree(best_sets[1])
-        return DecisionNode(col=split_criteria[0],
-                            value=split_criteria[1],
-                            lb = leftBranch,
-                            rb = rightBranch
+        return DecisionNode(col   = split_criteria[0],
+                            value = split_criteria[1],
+                            lb    = leftBranch,
+                            rb    = rightBranch
                             )
-    else: return DecisionNode(labels=count_labels(rows))
+    
+    else: return DecisionNode(labels=list(create_labels_dict(rows).keys())[0])
 
-# TODO:
-# def display_tree()
+
+def get_node_height(tree, node, height):
+    if not tree:
+        return 0
+    
+    if tree == node:
+        return height
+    
+    level = get_node_height(tree.left_branch, node, height + 1)
+
+    if level:
+        return level
+    
+    return get_node_height(tree.right_branch, node, height + 1)
+
+
+# level traversal
+def display_tree(tree):
+    
+    if not tree:
+        return
+    
+    order_q = []
+    order_q.append(tree)
+
+    while order_q:
+        node = order_q.pop(0)
+        if node.labels:
+            print(
+                f"Level {get_node_height(tree, node, 1)} : Leaf Node with Label {node.labels}")
+        else :
+            print(
+                f"Level {get_node_height(tree, node, 1)} : Split Node on column: {node.col} by value {node.value}")
+
+
+        if node.left_branch:
+            order_q.append(node.left_branch)
+        
+        if node.right_branch:
+            order_q.append(node.right_branch)
+
 
 def classify(obsertvation, tree):
     if tree.labels:
@@ -107,9 +149,15 @@ def main():
             ]
 
     tree = construct_tree(data)
-    label = classify(['direct', 'USA', 'yes', 5] , tree)
-    print(label)
-    return
+    
+    display_tree(tree)
+    
+    print("--------------------------------------------")
+    new_data_point = ['direct', 'USA', 'yes', 5]
+    label = classify(new_data_point, tree)
+    print(new_data_point, label)
+
+    return 0
 
 
 if __name__ == "__main__":
