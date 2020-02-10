c01cfb58645d72fe6dc94453ce91b19871da4f7b
sorindragan <sorin.dragan27@gmail.com>
2020-02-10 17:03:24 +0100

    refactored stack implementation

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -4,45 +4,52 @@ class Node:
         self.val = val
         self.next = None
 
+class Stack:
+
+    def __init__(self, init_val=None):
+        self.head = Node(init_val)
+
     def stack_status(self):
         print("Stack:")
-        node = self
+        node = self.head
         while node.next:
             print(node.val)
             node = node.next
         print(node.val)
 
     def push(self, val):
-        node = self
+        node = self.head
         if not node.val:
             node.val = val
             return node
         new_node = Node(val)
         new_node.next = node
-        return new_node
+        self.head = new_node
 
     def pop(self):
-        node = self
+        node = self.head
         pop_val = node.val
+        node.val = None
         if node.next:
             node = node.next
-            return pop_val, node
-        return pop_val, Node(None)
+            self.head = node
+            return pop_val
+        return pop_val
         
         
 
 def main():
-    stack = Node(1)
-    v, stack = stack.pop()
+    stack = Stack(1)
+    v = stack.pop()
     print("pop:", v)
-    stack = stack.push(2)
-    stack = stack.push(3)
-    stack = stack.push(4)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
     stack.stack_status()
-    v, stack = stack.pop()
+    v = stack.pop()
     print("pop:", v)
     stack.stack_status()
-    v, stack = stack.pop()
+    v = stack.pop()
     print("pop:", v)
     stack.stack_status()
 
